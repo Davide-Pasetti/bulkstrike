a9ded813f9ee99003fdca5a33827d8386f2aefa4
Davide Pasetti <davide@bulkstrike.com>
2026-07-03 16:08:46 +0200

    Corriere

--- proxy.ts
+++ proxy.ts
@@ -7,6 +7,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|welcome|registrati|pool|prodotto|fornitore|fornitori|dashboard|catalogo|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|welcome|registrati|pool|prodotto|fornitore|fornitori|dashboard|catalogo|corriere|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
   ],
 };
