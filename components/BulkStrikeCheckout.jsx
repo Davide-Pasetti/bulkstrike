9cc3b7b6770d327dc5afd647984756505f94c583
Davide Pasetti <davide@bulkstrike.com>
2026-07-06 23:14:24 +0200

    bug

--- components/BulkStrikeCheckout.jsx
+++ components/BulkStrikeCheckout.jsx
@@ -488,6 +488,7 @@ export default function CheckoutPage() {
                           </div>
                         </div>
                       </div>
+                      </div>
                     );
                   }
                   const sorted = [...q.quotes].sort((a, b) => {
